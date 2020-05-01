9062221904302a002236c94792b677277219cae6
Michelle Tran <mptran0101@berkeley.edu>
2020-05-01 14:40:45 -0700

    pseudo-code

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-console.log(moment().format());
+var $currentDay = $("#currentDay");
 
-var $currentDay = $("#currentDay")
 $currentDay.text(`today is ${moment().format("dddd, MMMM Do, YYYY")}`);
+
+// create object for each time block entry
+var myDay = {
+    hour: ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"],
+    event: "",
+};
+
+// create function that builds timeblocks
+
+// create something that takes in text input inside time block
+
+// create function that saves timeblock text input to local storage
+
+// create function that shows past/present/future
